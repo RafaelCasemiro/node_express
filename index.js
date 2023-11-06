e7822c2fc231b17f20fff30086dab420941486b2
EA - Rafael Dos Santos Borges Casemiro <borges_casemiro@educadventista.org>
2023-11-06 15:42:42 -0300

    recebendo informações do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -5,7 +5,27 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
-//https://localhost:3000/usuarios
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+
+//GET e POST
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`O email do usuário é: ${email}`)
+})
+
+app.get('/cadastrar', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
+//https://localhost:3000/usuarios/20    
 app.get('/usuarios/:id', (requisicao, resposta) => {
  const id = requisicao.params.id
 
@@ -18,6 +38,6 @@ app.get('/', (requisicao, resposta) => {
     resposta.sendFile (`${caminhoBase}/index.html`)
 })
 
-app.listen(3000, () => {
+app.listen(300, () => {
     console.log("Servidor rodando na porta 3000!")
 }) 
